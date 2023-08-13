97259e7a75f1fd6c478363263d435e23cb579a79
kistalex <kistalexander@yandex.ru>
2023-08-13 21:24:04 +0400

    Initial Commit

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//
+// MusicPlayer
+// ViewController.swift
+// 
+// Created by Alexander Kist on 13.08.2023.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
